f26accd7add81ca97bdaa0c1f594e109ce5fcb0e
VocalZero <kacper.mura@web.de>
2021-02-03 00:27:57 +0100

    Waiting for retrieved user data

--- src/main/java/eu/atomicnetworks/discordbot/commands/WhoisCommand.java
+++ src/main/java/eu/atomicnetworks/discordbot/commands/WhoisCommand.java
@@ -15,9 +15,8 @@ import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 
 /**
  *
- * @author Kacper Mura
- * 2021 Copyright (c) by atomicnetworks.eu to present.
- * All rights reserved. https://github.com/VocalZero
+ * @author Kacper Mura 2021 Copyright (c) by atomicnetworks.eu to present. All
+ * rights reserved. https://github.com/VocalZero
  *
  */
 public class WhoisCommand {
@@ -31,8 +30,8 @@ public class WhoisCommand {
     public void execute(GuildMessageReceivedEvent event) {
         Message message = event.getMessage();
         String[] args = message.getContentRaw().split(" ");
-        
-        if(!this.discord.getBackendManager().hasPermissionPower2(event.getMember())) {
+
+        if (!this.discord.getBackendManager().hasPermissionPower2(event.getMember())) {
             return;
         }
 
@@ -43,48 +42,53 @@ public class WhoisCommand {
             userId = (message.getMentionedMembers().stream().findFirst().orElse(null)).getId();
         }
         User user = this.discord.getBackendManager().getUser(userId);
-        
-        if(user.getId().equals(this.discord.getJda().getSelfUser().getId())) {
+
+        if (user.getId().equals(this.discord.getJda().getSelfUser().getId())) {
             return;
         }
-        if(user.getId().equals("697517106287345737")) {
+        if (user.getId().equals("697517106287345737")) {
             return;
         }
-        
+
         EmbedBuilder embed = new EmbedBuilder();
         embed.setColor(new Color(149, 79, 180));
         this.discord.getJda().retrieveUserById(user.getId()).queue((t1) -> {
-            embed.setAuthor(t1.getName(), null, t1.getAvatarUrl());
-        });
-        embed.setDescription(MessageFormat.format("The user {0} is currently **level {1}** and has **{2} warning points**.\n", "<@" + user.getId() + ">", user.getLevel(), String.valueOf(user.getWarn().getWarnPoints())) +
-                MessageFormat.format("He has already listened **{0} minutes** to music and has a total of **{1} votes**.", String.valueOf(user.getStreamTimeMin()), String.valueOf(user.getVoting().getVoteCount())));
-        String warnlog = "";
-        List<User.Warn.WarnLog> logs = user.getWarn().getWarnLog();
-        if(!logs.isEmpty()) {
-            Collections.reverse(logs);
-            for(User.Warn.WarnLog log : logs) {
-                SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.YYYY HH:mm");
-                if(null != log.getWarnType()) switch (log.getWarnType()) {
-                    case WARN:
-                        warnlog += MessageFormat.format("{0} I {1} » has issued a **warn**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
-                        break;
-                    case MUTE:
-                        warnlog += MessageFormat.format("{0} I {1} » has issued a **mute**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
-                        break;
-                    case BAN:
-                        warnlog += MessageFormat.format("{0} I {1} » has issued a **ban**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
-                        break;
-                    default:
-                        warnlog += MessageFormat.format("{0} I {1} » has issued a **???**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
-                        break;
-                }
+            if(t1 == null) {
+                System.out.println("USER IS NULL!");
+                return;
             }
-            embed.addBlankField(true);
-            embed.addField(new MessageEmbed.Field("**Warnlog:**", warnlog, false));
-        }
+            embed.setAuthor(t1.getName(), null, t1.getAvatarUrl());
+            embed.setDescription(MessageFormat.format("The user {0} is currently **level {1}** and has **{2} warning points**.\n", "<@" + user.getId() + ">", user.getLevel(), String.valueOf(user.getWarn().getWarnPoints()))
+                    + MessageFormat.format("He has already listened **{0} minutes** to music and has a total of **{1} votes**.", String.valueOf(user.getStreamTimeMin()), String.valueOf(user.getVoting().getVoteCount())));
+            String warnlog = "";
+            List<User.Warn.WarnLog> logs = user.getWarn().getWarnLog();
+            if (!logs.isEmpty()) {
+                Collections.reverse(logs);
+                for (User.Warn.WarnLog log : logs) {
+                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd.MM.YYYY HH:mm");
+                    if (null != log.getWarnType()) {
+                        switch (log.getWarnType()) {
+                            case WARN:
+                                warnlog += MessageFormat.format("{0} I {1} » has issued a **warn**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
+                                break;
+                            case MUTE:
+                                warnlog += MessageFormat.format("{0} I {1} » has issued a **mute**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
+                                break;
+                            case BAN:
+                                warnlog += MessageFormat.format("{0} I {1} » has issued a **ban**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
+                                break;
+                            default:
+                                warnlog += MessageFormat.format("{0} I {1} » has issued a **???**.\n", simpleDateFormat.format(new Date(log.getStart_at())), "<@" + log.getCreator() + ">");
+                                break;
+                        }
+                    }
+                }
+                embed.addField(new MessageEmbed.Field("**Warnlog:**", warnlog, false));
+            }
 
-        event.getMember().getUser().openPrivateChannel().queue((channel) -> {
-            channel.sendMessage(embed.build()).queue();
+            event.getMember().getUser().openPrivateChannel().queue((channel) -> {
+                channel.sendMessage(embed.build()).queue();
+            });
         });
     }
 
