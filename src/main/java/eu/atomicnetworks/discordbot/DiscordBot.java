30152c9085386b54c02e84a1a58f9c4523c1b160
Henrik Martens <henrik.m@atomicnetworks.eu>
2021-03-10 01:42:05 +0100

    Update dashboard message #2
    Updated dashboard message #2

--- src/main/java/eu/atomicnetworks/discordbot/DiscordBot.java
+++ src/main/java/eu/atomicnetworks/discordbot/DiscordBot.java
@@ -110,7 +110,7 @@ public class DiscordBot {
                 EmbedBuilder embed = new EmbedBuilder();
                 embed.setColor(new Color(149, 79, 180));
                 embed.setAuthor("Supportsystem", null, "https://cdn.atomicnetworks.eu/discord/icon.png");
-                embed.setDescription("You have a problem, want to send us feedback or something else to our support? By clicking on the emoji below, you can open a ticket where our team will be happy to help you.\n\n"
+                embed.setDescription("You have a problem, want to send us feedback or something else to our support?\nBy clicking on the emoji below, you can open a ticket where our team will be happy to help you.\n\n"
                         + "<:atomic:736627104992591883> atomicnetworks.eu\n"
                         + "<:playatomic:734613241581404271> atomicradio.eu\n"
                         + "<:gamingatomic:734611793187700736> atomicgaming.eu");
